df378f8c59b06c926ea9b6d8b47875d5d65411a0
al.barinov <al.barinov@academcity.online>
2026-09-24 20:34:11 +0300

    feat: Подключен symfony к проекту

---
+++ src/Controller/HealthController.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/api/health', name: 'api_health', methods: ['GET'])]
+    public function __invoke(): JsonResponse
+    {
+        return $this->json(['status' => 'ok'], Response::HTTP_OK);
+    }
+}
